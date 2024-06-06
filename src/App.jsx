4bd15c77e4f1cee4e05cca2edbd68d65970db348
MonizaBritto <monyza2017@gmail.com>
2024-06-05 23:26:51 -0300

    Teste

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import {createGlobalStyle} from "styled-components"
+import Inicio from "./Components/Inicio/inicio.jsx"
+
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
+
+function App() {
+  return(
+    <>
+    <GlobalStyle/>
+    <Inicio/>
+    </>
+  )
+  
+
+}
+  
+
+export default App
